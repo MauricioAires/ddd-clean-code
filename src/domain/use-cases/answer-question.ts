b343e62e6e9d2000c790ea33fb2a3f061cb1c34b
MauricioAires <mauricioaires015@gmail.com>
2023-12-09 16:50:46 -0300

    feat(core) crate use cases

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,28 @@
+import { Answer } from "../entities/answer";
+import { AnswersRepository } from "../repositories/answers-repository";
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string;
+  questionId: string;
+  content: string;
+}
+
+// DRY - Don't	Repeat Yourself (3x considerar refatorar)
+export class AnswerQuestionUseCase {
+  constructor(private answerRepository: AnswersRepository) {}
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer({
+      authorId: instructorId,
+      questionId,
+      content,
+    });
+
+    this.answerRepository.create(answer);
+
+    return answer;
+  }
+}
