7ed88f0d92060f4c8978f50d8e2a477fab4df757
MauricioAires <mauricioaires015@gmail.com>
2023-12-13 23:10:02 -0300

    feat(core) create response handler

---
+++ src/core/either.spec.ts
@@ -0,0 +1,27 @@
+import { Either, left, right } from './either'
+
+function doSomething(shouldSuccess: boolean): Either<string, number> {
+  if (shouldSuccess) {
+    return right(10)
+  }
+
+  return left('error')
+}
+
+test.only('success result', () => {
+  const result = doSomething(true)
+
+  if (result.isRight()) {
+    console.log(result.value)
+  }
+
+  expect(result.isRight()).toBeTruthy()
+  expect(result.isLeft()).toBeFalsy()
+})
+
+test.only('error result', () => {
+  const result = doSomething(false)
+
+  expect(result.isLeft()).toBeTruthy()
+  expect(result.isRight()).toBeFalsy()
+})
