850af258f6284ed199586aaf857f5d41970c9321
MauricioAires <mauricioaires015@gmail.com>
2023-12-13 23:06:09 -0300

    feat(test) test mock repositories

---
+++ src/test/repositories/in-memory-question-repository.ts
@@ -0,0 +1,51 @@
+import { PaginationParams } from '@/core/repositories/paginate-params'
+import { QuestionRepository } from '@/domain/forum/application/repositories/question-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+
+export class InMemoryQuestionsRepository implements QuestionRepository {
+  public items: Question[] = []
+
+  async create(data: Question): Promise<void> {
+    this.items.push(data)
+  }
+
+  async findBySlug(slug: string): Promise<Question | null> {
+    const question = this.items.find((item) => item.slug.value === slug)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
+  async findById(id: string): Promise<Question | null> {
+    const question = this.items.find((item) => item.id.toString() === id)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
+  async delete(question: Question): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items.splice(itemIndex, 1)
+  }
+
+  async save(data: Question): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === data.id)
+
+    this.items[itemIndex] = data
+  }
+
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createAt.getTime() - a.createAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
+  }
+}
