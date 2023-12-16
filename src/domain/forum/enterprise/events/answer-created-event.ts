dfe34d8a80bbd0963873839f8823d464ccb3953f
MauricioAires <mauricioaires015@gmail.com>
2023-12-16 19:34:14 -0300

    feat(forum) event domain signature

---
+++ src/domain/forum/enterprise/events/answer-created-event.ts
@@ -0,0 +1,19 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { DomainEvent } from '@/core/events/domain-event'
+import { Answer } from '../entities/answer'
+
+export class AnswerCreatedEvent implements DomainEvent {
+  ocurredAt: Date
+  answer: Answer
+
+  constructor(answer: Answer) {
+    this.ocurredAt = new Date()
+    this.answer = answer
+  }
+
+  getAggregateId(): UniqueEntityID {
+    return this.answer.id
+  }
+}
+
+/// O que são classes agregadas ??
